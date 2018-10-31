5b5ead24d19399ee90ed03e76bd86dc6aab71a41
sapuri <info@minami.xyz>
2018-10-31 14:41:17 +0900

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,13 +6,14 @@
       <a href="https://web3js.readthedocs.io/en/1.0/index.html" target="_blank" rel="noopener">web3.js</a>
       for support your dapps development
     </p>
+    <p><a href="https://github.com/sapuri/web3-online-tools" target="_blank" rel="noopener">GitHub repository</a></p>
     <h3>Convert to Checksum Address</h3>
     <p>web3.utils.toChecksumAddress([address])</p>
     <div>
       <input v-model="address" placeholder="0x...">
       <br>
       <button v-on:click="convert">Convert</button>
-      <p v-if="checksumAddress">Checksum Address: {{ checksumAddress }}</p>
+      <p v-if="checksumAddress" class="result">Checksum Address: {{ checksumAddress }}</p>
       <p v-if="errorMsg" class="error-msg">{{ errorMsg }}</p>
     </div>
   </div>
@@ -62,6 +63,10 @@
 
   a {
     color: #42b983;
+  }
+
+  .result {
+    color: green;
   }
 
   .error-msg {
