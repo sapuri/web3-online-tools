b6e68c668a0a2ff5d26e924a54136b75a5d5b859
sapuri <info@minami.xyz>
2018-11-01 20:50:46 +0900

    refactor: add component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div id="home">
     <h1>Web3 Online Tools</h1>
     <p>
       Online web3 tools using
@@ -7,64 +7,24 @@
       for support your dapps development
     </p>
     <p><a href="https://github.com/sapuri/web3-online-tools" target="_blank" rel="noopener">GitHub repository</a></p>
-    <h3>Convert to Checksum Address</h3>
-    <p>web3.utils.toChecksumAddress([address])</p>
-    <div>
-      <input v-model="address" placeholder="0x...">
-      <br>
-      <button v-on:click="convert">Convert</button>
-      <p v-if="checksumAddress" class="result">Checksum Address: {{ checksumAddress }}</p>
-      <p v-if="errorMsg" class="error-msg">{{ errorMsg }}</p>
-    </div>
+
+    <ToChecksumAddress/>
   </div>
 </template>
 
 <script>
-  import * as web3 from "web3";
+  import ToChecksumAddress from './ToChecksumAddress.vue'
 
   export default {
-    name: 'Home',
-    data() {
-      return {
-        address: '',
-        checksumAddress: '',
-        errorMsg: ''
-      }
-    },
-    methods: {
-      convert: function () {
-        this.checksumAddress = '';
-        this.errorMsg = '';
-        try {
-          this.checksumAddress = web3.utils.toChecksumAddress(this.address);
-        } catch (e) {
-          this.errorMsg = e.toString();
-        }
-      }
+    name: 'home',
+    components: {
+      ToChecksumAddress
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
-
+<style>
   .result {
     color: green;
   }
---
+++ src/components/ToChecksumAddress.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="to-checksum-address">
+    <h3>Convert to Checksum Address</h3>
+    <p>web3.utils.toChecksumAddress([address])</p>
+    <div>
+      <input v-model="address" placeholder="0x...">
+      <br>
+      <button v-on:click="run">Run</button>
+      <p v-if="checksumAddress" class="result">Checksum Address: {{ checksumAddress }}</p>
+      <p v-if="errorMsg" class="error-msg">{{ errorMsg }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import * as web3 from "web3";
+
+  export default {
+    name: 'to-checksum-address',
+    data() {
+      return {
+        address: '',
+        checksumAddress: '',
+        errorMsg: ''
+      }
+    },
+    methods: {
+      run: function () {
+        this.checksumAddress = '';
+        this.errorMsg = '';
+        try {
+          this.checksumAddress = web3.utils.toChecksumAddress(this.address);
+        } catch (e) {
+          this.errorMsg = e.toString();
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
