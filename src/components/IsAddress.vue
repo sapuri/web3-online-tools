4c513edbaee4d27199323e78d6a360ecd432c64e
sapuri <info@minami.xyz>
2018-11-03 14:14:12 +0900

    materialize components

--- src/components/IsAddress.vue
+++ src/components/IsAddress.vue
@@ -1,22 +1,40 @@
 <template>
   <div id="is-address">
-    <h3>isAddress</h3>
-    <pre>web3.utils.isAddress(address)</pre>
-    <p class="description">Checks if a given string is a valid Ethereum address. It will also check the checksum, if the address has upper
-      and lowercase letters.</p>
-    <div>
-      <label for="address">address - String: An address string.</label><br>
-      <input id="address" v-model="address" placeholder="address">
-      <br>
-      <button v-on:click="run">Run</button>
-      <p v-if="result" class="result">Result: {{ result }}</p>
-      <p v-if="errorMsg" class="error-msg">{{ errorMsg }}</p>
-    </div>
+    <md-card>
+      <md-card-header>
+        <div class="md-title">isAddress</div>
+      </md-card-header>
+
+      <md-card-content>
+        <pre>web3.utils.isAddress(address)</pre>
+        <p class="description">Checks if a given string is a valid Ethereum address. It will also check the checksum, if
+          the address has upper
+          and lowercase letters.</p>
+        <div>
+          <md-field>
+            <label>address</label>
+            <md-input v-model="address"></md-input>
+            <span class="md-helper-text">String: An address string.</span>
+          </md-field>
+          <p v-if="result" class="result">Result: {{ result }}</p>
+          <p v-if="errorMsg" class="error-msg">{{ errorMsg }}</p>
+        </div>
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button v-on:click="run">Run</md-button>
+      </md-card-actions>
+    </md-card>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+  import {MdCard, MdField} from 'vue-material/dist/components';
   import * as web3 from "web3";
+
+  Vue.use(MdCard);
+  Vue.use(MdField);
 
   export default {
     name: 'is-address',
--- src/components/ToChecksumAddress.vue
+++ src/components/ToChecksumAddress.vue
@@ -1,21 +1,38 @@
 <template>
   <div id="to-checksum-address">
-    <h3>toChecksumAddress</h3>
-    <pre>web3.utils.toChecksumAddress(address)</pre>
-    <p class="description">Will convert an upper or lowercase Ethereum address to a checksum address.</p>
-    <div>
-      <label for="address">address - String: An address string.</label><br>
-      <input id="address" v-model="address" placeholder="address">
-      <br>
-      <button v-on:click="run">Run</button>
-      <p v-if="result" class="result">Result: {{ result }}</p>
-      <p v-if="errorMsg" class="error-msg">{{ errorMsg }}</p>
-    </div>
+    <md-card>
+      <md-card-header>
+        <div class="md-title">toChecksumAddress</div>
+      </md-card-header>
+
+      <md-card-content>
+        <pre>web3.utils.toChecksumAddress(address)</pre>
+        <p class="description">Will convert an upper or lowercase Ethereum address to a checksum address.</p>
+        <div>
+          <md-field>
+            <label>address</label>
+            <md-input v-model="address"></md-input>
+            <span class="md-helper-text">String: An address string.</span>
+          </md-field>
+          <p v-if="result" class="result">Result: {{ result }}</p>
+          <p v-if="errorMsg" class="error-msg">{{ errorMsg }}</p>
+        </div>
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button v-on:click="run">Run</md-button>
+      </md-card-actions>
+    </md-card>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+  import {MdCard, MdField} from 'vue-material/dist/components';
   import * as web3 from "web3";
+
+  Vue.use(MdCard);
+  Vue.use(MdField);
 
   export default {
     name: 'to-checksum-address',
